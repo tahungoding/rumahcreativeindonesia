fd57688efe666faf5fd31d92fafb8a91fb4b458a
bagaspardanailham18 <ilhamp.dev@gmail.com>
2021-01-19 11:16:34 +0700

    Update Routes

--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,22 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+Route::group(['middleware' => 'auth'], function () {
+    Route::resource('agenda', 'AgendaController');
+    Route::resource('artikel', 'ArtikelController');
+    Route::resource('home', 'HomeController');
+    Route::resource('kategori_artikel', 'KategoriArtikelController');
+    Route::resource('kategori_mitra', 'KategoriMitraController');
+    Route::resource('kategori_umkm', 'KategoriUmkmController');
+    Route::resource('mitra', 'MitraController');
+    Route::resource('profile', 'ProfileController');
+    Route::resource('program', 'ProgramController');
+    Route::resource('struktur_tim', 'StrukturTimController');
+    Route::resource('testimoni', 'TestimoniController');
+    Route::resource('umkm', 'UmkmController');
+    Route::resource('user', 'UserController');
+    Route::resource('user_level', 'UserLevelController');
+});
 
 Auth::routes();
 
