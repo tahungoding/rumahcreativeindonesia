336c735ab4b818bcc1501735b6f3eb801eed6ba7
Ramdani Surya M <a2.1800105@mhs.stmik-sumedang.ac.id>
2021-02-10 09:20:58 +0700

    image preview di struktur tim

--- resources/views/guest/profile.blade.php
+++ resources/views/guest/profile.blade.php
@@ -17,7 +17,7 @@
             </div>
         </section>
         <!-- Page Title End -->
-        
+
         <!-- About Section Begin -->
         <section class="pt-120 pb-120">
             <div class="container">
@@ -34,24 +34,24 @@
                         <!-- Section Title End -->
                     </div>
                 </div>
-        
+
                 <div class="row">
                     <div class="col-12">
-                        <img src="{{avatar($profile->latar_belakang_img)}}" data-rjs="2" alt="" style="width: 1112px;height: 460px;object-fit: cover" >
+                        <img src="{{avatar($profile['latar_belakang_img'])}}" data-rjs="2" alt="" style="width: 1112px;height: 460px;object-fit: cover" >
                     </div>
                 </div>
-        
+
                 <div class="row justify-content-center">
                     <div class="col-lg-10">
                         <div class="about-text mt-5">
-                            {!! $profile->latar_belakang !!}
+                            {!! $profile['latar_belakang'] !!}
                         </div>
                     </div>
                 </div>
             </div>
         </section>
         <!-- About Section End -->
-        
+
         <!-- About Section Begin -->
         <section class="pt-120 pb-120 ov-hidden">
             <div class="container">
@@ -60,14 +60,14 @@
                         <!-- Section Title Begin -->
                         <div class="section-title mb-4">
                             <h2>VISI</h2>
-                            {!! $profile->visi !!}
+                            {!! $profile['visi'] !!}
                         </div>
                         <!-- Section Title End -->
-        
+
                     </div>
                     <!-- Video Area -->
                     <div class="col-lg-5 video-area mt-50 mt-lg-0">
-                        <img src="{{avatar($profile->visi_img)}}" data-rjs="2" alt="" style="max-height: 500px;">
+                        <img src="{{avatar($profile['visi_img'])}}" data-rjs="2" alt="" style="max-height: 500px;">
                         {{-- <a href="#" class="vdo-btn popup-video"><img src="{{asset('assets/front/img/icons/play.svg')}}" class="svg" alt=""> Watch
                             Video</a> --}}
                     </div>
@@ -76,29 +76,29 @@
             </div>
         </section>
         <!-- About Section Begin -->
-        
+
         <!-- About Section Begin -->
         <section class="pt-120 pb-120">
             <div class="container">
                 <div class="row align-items-center">
                     <div class="col-lg-6">
-                        <img src="{{avatar($profile->misi_img)}}" data-rjs="2" class="w-100" alt="">
+                        <img src="{{avatar($profile['misi_img'])}}" data-rjs="2" class="w-100" alt="">
                     </div>
-        
+
                     <div class="col-lg-6">
                         <!-- Section Title Begin -->
                         <div class="section-title mb-4 mt-50 mt-lg-0">
                             <h2>MISI</h2>
-                            {!! $profile->misi !!}
+                            {!! $profile['misi'] !!}
                         </div>
                         <!-- Section Title End -->
-        
+
                     </div>
                 </div>
             </div>
         </section>
         <!-- About Section End -->
-        
+
         <!-- Counter Begin -->
 <section class="pt-120 pb-70 gradient-bg">
         <div class="container">
@@ -111,7 +111,7 @@
                     </div>
                 </div>
                 <!-- End Single Counter -->
-    
+
                 <!-- Single Counter -->
                 <div class="col-lg-4 col-sm-6">
                     <div class="single-counter text-center text-white">
@@ -120,7 +120,7 @@
                     </div>
                 </div>
                 <!-- End Single Counter -->
-    
+
                 <!-- Single Counter -->
                 <div class="col-lg-4 col-sm-6">
                     <div class="single-counter text-center text-white">
@@ -129,7 +129,7 @@
                     </div>
                 </div>
                 <!-- End Single Counter -->
-    
+
                 <!-- Single Counter
                         <div class="col-lg-3 col-sm-6">
                             <div class="single-counter text-center text-white">
@@ -142,7 +142,7 @@
         </div>
     </section>
         <!-- Counter End -->
-        
+
         <!-- About Section Begin -->
         <section class="pt-120 pb-120">
             <div class="container">
@@ -155,24 +155,24 @@
                         <!-- Section Title End -->
                     </div>
                 </div>
-        
+
                 <div class="row">
                     <div class="col-12">
-                        <img src="{{avatar($profile->model_konsep_img)}}" data-rjs="2" alt="" style="width: 1112px;height: 460px;object-fit: cover">
+                        <img src="{{avatar($profile['model_konsep_img'])}}" data-rjs="2" alt="" style="width: 1112px;height: 460px;object-fit: cover">
                     </div>
                 </div>
-        
+
                 <div class="row justify-content-center">
                     <div class="col-lg-10">
                         <div class="about-text mt-5">
-                            {!! $profile->kekuatan !!}
+                            {!! $profile['kekuatan'] !!}
                         </div>
                     </div>
                 </div>
             </div>
         </section>
         <!-- About Section End -->
-        
+
         <!-- Team Begin -->
         <section class="pt-120 pb-70 section-pattern" data-bg-img="{{asset('assets/front/img/section-pattern/testimonial-pattern.png')}}">
             <div class="container">
@@ -185,7 +185,7 @@
                         <!-- Section Title End -->
                     </div>
                 </div>
-        
+
                 <div class="row">
                     @foreach ($struktur_tim as $item)
                         <div class="col-lg-4 col-sm-6">
@@ -193,18 +193,18 @@
                             <div class="single-team-member">
                                 <!-- Team Image Begin -->
                                 <div class="image position-relative">
-            
+
                                     <img src="{{avatar($item->foto)}}" data-rjs="2" alt="" style="width: 348px;height: 283px;object-fit: cover">
                                 </div>
                                 <!-- Team Image End -->
-            
+
                                 <!-- Team Info Begin -->
                                 <div class="team-info">
                                     <div class="info-front text-center">
                                         <h4>{{$item->nama}}</h4>
                                         <p>{{$item->jabatan}}</p>
                                     </div>
-            
+
                                     <div class="info-back">
                                         <ul class="social_icon_list list-inline text-center w-100 d-flex justify-content-around">
                                             <li>
@@ -231,8 +231,8 @@
             </div>
         </section>
         <!-- Team End -->
-        
-        
+
+
         <!-- Testimonial Begin -->
         <section class="pt-120 pb-120 section-pattern" data-bg-img="{{asset('assets/front/img/section-pattern/testimonial-pattern.png')}}">
             <div class="container">
@@ -245,7 +245,7 @@
                         <!-- Section Title End -->
                     </div>
                 </div>
-        
+
                 <!-- Testimonial Slider Begin -->
                 <div class="testimonial-slider owl-carousel" data-owl-nav="true" data-owl-autoplay="false"
                     data-owl-animate-Out="fadeOut" data-owl-animate-in="fadeIn" data-owl-margin="3">
@@ -260,12 +260,12 @@
                                     </div>
                                     <!-- Testimonial Image End -->
                                 </div>
-            
+
                                 <div class="col-md-7">
                                     <!-- Testimonial Content Begin -->
                                     <div class="testimonial-content">
                                         <p>{{$item->isi}}</p>
-            
+
                                         <h4>{{$item->responden}}</h4>
                                         <span>{{$item->asal}}</span>
                                     </div>
@@ -280,7 +280,7 @@
             </div>
         </section>
         <!-- Testimonial End -->
-        
+
         <!-- Brand Slider Begin -->
         <section class="pt-120 pb-120">
             <div class="container">
@@ -293,7 +293,7 @@
                                     <img src="{{avatar($item->logo)}}" data-rjs="2" alt="" style="max-height: 100px; object-fit:cover;">
                                 </a>
                             @endforeach
-        
+
                         </div>
                         <!-- End of Partners -->
                     </div>
@@ -301,8 +301,8 @@
             </div>
         </section>
         <!-- Brand Slider End -->
-        
-        
+
+
         <!-- Contact Info Begin -->
         <section class="pt-120 pb-120">
             <div class="container">
@@ -320,11 +320,11 @@
                                     </div>
                                     <div class="media-body">
                                         <h4>Lokasi Kantor</h4>
-                                        {!! $profile->alamat !!}
+                                        {!! $profile['alamat'] !!}
                                     </div>
                                 </div>
                                 <!-- End Single Contact Info -->
-        
+
                                 <!-- Single Contact Info -->
                                 <div class="col-sm-6 col-lg-12 single-contact-info media align-items-center">
                                     <div class="image">
@@ -332,11 +332,11 @@
                                     </div>
                                     <div class="media-body">
                                         <h4>Telepon</h4>
-                                        {!! $profile->telepon !!}
+                                        {!! $profile['telepon'] !!}
                                     </div>
                                 </div>
                                 <!-- End Single Contact Info -->
-        
+
                                 <!-- Single Contact Info -->
                                 <div class="col-sm-6 col-lg-12 single-contact-info media align-items-center">
                                     <div class="image">
@@ -344,11 +344,11 @@
                                     </div>
                                     <div class="media-body">
                                         <h4>Email</h4>
-                                        {!! $profile->email !!}
+                                        {!! $profile['email'] !!}
                                     </div>
                                 </div>
                                 <!-- End Single Contact Info -->
-        
+
                                 <!-- Single Contact Info -->
                                 <div class="col-sm-6 col-lg-12 single-contact-info media align-items-center">
                                     <div class="image">
@@ -356,18 +356,18 @@
                                     </div>
                                     <div class="media-body">
                                         <h4>Jam Kerja</h4>
-                                        {!! $profile->jam_kerja !!}
+                                        {!! $profile['jam_kerja'] !!}
                                     </div>
                                 </div>
                                 <!-- End Single Contact Info -->
-        
+
                             </div>
                         </div>
                         <!-- Contact Info End -->
                     </div>
-        
+
                     <div class="col-lg-8">
-        
+
                         <!-- Start Map -->
                         {{-- <div class="map" data-trigger="map"
                             data-map-options='{"latitude": "40.6785635", "longitude": "-73.9664109", "zoom": "11"}'>
