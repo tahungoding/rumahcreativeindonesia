44425bc47a0b2afa8e80c2d2ed54c23e4767ea02
Ramdani Surya M <a2.1800105@mhs.stmik-sumedang.ac.id>
2021-02-11 13:53:50 +0700

    image preview di setting, profile dan slider

--- resources/views/slider/edit.blade.php
+++ resources/views/slider/edit.blade.php
@@ -34,8 +34,11 @@
                     </div>
 
                     <div class="form-group">
-                        <label for="">Gambar</label>
-                        <input type="file" class="filestyle" data-buttonname="btn-secondary" name="gambar" id="" >
+                        <label>Gambar</label><br>
+                        <img class="rounded img-preview mr-2 mo-mb-2" alt="200x200" width="200" src="{{ isset($slider) ? avatar($slider->gambar) : avatar() }}" data-holder-rendered="true">
+                    </div>
+                    <div class="form-group">
+                        <input type="file" class="filestyle" data-buttonname="btn-secondary" name="gambar" id="" onchange="filePreview(this)">
                         @error('gambar')
                             <span class="text-danger">{{ $message}}</span>
                         @enderror
